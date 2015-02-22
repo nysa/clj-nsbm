827751439210922017e2dad4ed0d2927fbb9d795
Nysa <nysa@nysavann.com>
2015-02-21 21:03:18 -0600

    Add project.clj and LICENSE

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject com.nysavann/clj-nsbm "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :source-paths ["src/clj"]
+  :test-paths ["test/clj"])
