0ab536ccb7b2f6127d405b02029fcdfe79d85eb9
Nysa <nysa@nysavann.com>
2015-02-25 23:00:45 -0600

    WIP: Add clj-nsbm/subfolder

--- src/clj/clj_nsbm/core.clj
+++ src/clj/clj_nsbm/core.clj
@@ -22,6 +22,15 @@
        (map #(list ((first %) keys-attrs) (last %))) flatten
        (apply hash-map)))
 
+(defn subfolder
+  [item]
+  (let [keys-attrs {:date :ADD_DATE}]
+    (str "<DT><H3 FOLDED"
+         (attrs->str (item->attrs item keys-attrs)) ">"
+         (:title item) "</H3><DL><p>"
+         ;; TODO: Build children as string
+         "</DL><p>")))
+
 (defn shortcut
   [item]
   (let [keys-attrs {:url :HREF, :date :ADD_DATE}]
