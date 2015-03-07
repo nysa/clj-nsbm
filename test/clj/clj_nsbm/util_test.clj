a4eaac8a569c558bc08d3f9d16950f02c93bc2c8
Nysa <nysa@nysavann.com>
2015-03-07 16:12:50 -0600

    Add attr-str-test and attrs-str-test

--- test/clj/clj_nsbm/util_test.clj
+++ test/clj/clj_nsbm/util_test.clj
@@ -2,6 +2,14 @@
   (:require [clojure.test :refer :all]
             [clj-nsbm.util :refer :all]))
 
+(deftest attr-str-test
+  (is (= (attr-str ["href" "http://clojure.org"]) " href=\"http://clojure.org\""))
+  (is (= (attr-str [:href  "http://clojure.org"]) " href=\"http://clojure.org\"")))
+
+(deftest attrs-str-test
+  (is (= (attrs-str {:title "Clojure"}) " title=\"Clojure\""))
+  (is (= (attrs-str {:title "Clojure" :href "http://clojure.org"}) " title=\"Clojure\" href=\"http://clojure.org\"")))
+
 (deftest item->attrs-test
   (is (= (item->attrs {:url "http://clojure.org/"} {:url :HREF}) {:HREF "http://clojure.org/"}))
   (is (= (item->attrs {:url "http://clojure.org/"} {:url :HREF, :date :ADD_DATE}) {:HREF "http://clojure.org/"}))
