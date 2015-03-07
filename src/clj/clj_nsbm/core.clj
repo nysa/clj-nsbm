674c5f48a1ed6849cfde105706ea365ddfb9f533
Nysa <nysa@nysavann.com>
2015-03-07 15:31:53 -0600

    Split clj-nsbm.core/attrs->str into two separate functions

--- src/clj/clj_nsbm/core.clj
+++ src/clj/clj_nsbm/core.clj
@@ -7,8 +7,8 @@
 
 (def head
   (str "<!DOCTYPE NETSCAPE-Bookmark-file-1>"
-       "<!--This is an automatically generated file."
-       "It will be read and overwritten."
+       "<!--This is an automatically generated file. "
+       "It will be read and overwritten. "
        "Do Not Edit! -->"
        "<Title>Bookmarks</Title>"
        "<H1>Bookmarks</H1>"
@@ -16,9 +16,18 @@
 
 (def foot "</DL>")
 
-(defn attrs->str
-  [attrs]
-  (string/join (map #(str " " (name (first %)) "=\"" (last %) "\"") attrs)))
+(defn- attr-str
+  "Returns the string representation of a given vector as an HTML attribute
+  key/value pair where the first item is the attribute name and the second
+  item is the attribute value."
+  [v]
+  (str " " (name (first v)) "=\"" (str (second v)) "\""))
+
+(defn- attrs-str
+  "Returns the string representation of a given map as HTML attribute
+  key/value pairs"
+  [m]
+  (->> m (map attr-str) string/join))
 
 (defn item->attrs
   [item, keys-attrs]
@@ -34,7 +43,7 @@
   [item]
   (let [keys-attrs {:date :ADD_DATE}]
     (str "<DT><H3 FOLDED"
-         (attrs->str (item->attrs item keys-attrs)) ">"
+         (attrs-str (item->attrs item keys-attrs)) ">"
          (:title item) "</H3><DL><p>"
          (string/join (map item->str (:children item)))
          "</DL><p>")))
@@ -42,5 +51,5 @@
 (defn shortcut
   [item]
   (let [keys-attrs {:url :HREF, :date :ADD_DATE}]
-    (str "<DT><A" (attrs->str (item->attrs item keys-attrs)) ">"
+    (str "<DT><A" (attrs-str (item->attrs item keys-attrs)) ">"
          (:title item) "</A>")))
--- test/clj/clj_nsbm/core_test.clj
+++ test/clj/clj_nsbm/core_test.clj
@@ -2,10 +2,6 @@
   (:require [clojure.test :refer :all]
             [clj-nsbm.core :refer :all]))
 
-(deftest attrs->str-test
-  (is (= (attrs->str {:foo "bar"}) " foo=\"bar\"")))
-  (is (= (attrs->str {:foo "bar" :bar "baz"}) " bar=\"baz\" foo=\"bar\""))
-
 (deftest item->attrs-test
   (is (= (item->attrs {:url "http://clojure.org/"} {:url :HREF}) {:HREF "http://clojure.org/"}))
   (is (= (item->attrs {:url "http://clojure.org/"} {:url :HREF, :date :ADD_DATE}) {:HREF "http://clojure.org/"}))
