1b13719c65cc90173ecdd54292bcc16d210e1a11
Nysa <nysa@nysavann.com>
2015-03-15 12:50:45 -0500

    Update description and url in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.nysavann/clj-nsbm "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Library for building Netscape Bookmark files"
+  :url "https://github.com/nysa/clj-nsbm"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
